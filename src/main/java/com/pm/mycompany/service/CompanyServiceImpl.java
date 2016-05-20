44be5d064c4c453e7147d837d6d0c381c7197f55
Pawel M <pawmac24@gmail.com>
2016-05-20 12:26:52 +0200

    Adding simple business logic to company service

--- src/main/java/com/pm/mycompany/service/CompanyServiceImpl.java
+++ src/main/java/com/pm/mycompany/service/CompanyServiceImpl.java
@@ -19,32 +19,60 @@ public class CompanyServiceImpl implements CompanyService{
 
     public final static Logger logger = Logger.getLogger(CompanyServiceImpl.class);
 
+    private List<CompanyResultDTO> companyResultDTOList = new ArrayList<>();
+    private Long initialId = 10L;
+
     @Override
     public CompanyPrimaryKeyDTO create(CompanyCreateDTO companyDTO) throws BusinessValidationException {
         logger.info("create company");
-        CompanyPrimaryKeyDTO companyPKeyDTO = new CompanyPrimaryKeyDTO(10L);
+        CompanyPrimaryKeyDTO companyPKeyDTO = new CompanyPrimaryKeyDTO(initialId);
+        CompanyResultDTO companyResultDTO = new CompanyResultDTO();
+        companyResultDTO.setName(companyDTO.getName());
+        companyResultDTO.setCompanyId(companyPKeyDTO.getId());
+        companyResultDTO.setAddress(companyDTO.getAddress());
+        companyResultDTOList.add(companyResultDTO);
+        initialId++;
         return companyPKeyDTO;
     }
 
     @Override
     public void update(Long companyId, CompanyUpdateDTO companyDTO) throws BusinessValidationException {
         logger.info("update company");
+        for (CompanyResultDTO company: companyResultDTOList) {
+            if (company.getCompanyId().equals(companyId)){
+                company.setName(companyDTO.getName());
+                company.setAddress(companyDTO.getAddress());
+            }
+        }
     }
 
     @Override
     public CompanyResultDTO find(Long companyId) throws BusinessValidationException {
         CompanyResultDTO companyResultDTO = new CompanyResultDTO();
+        for (CompanyResultDTO company: companyResultDTOList) {
+            if (company.getCompanyId().equals(companyId)){
+                companyResultDTO = company;
+            }
+        }
         return companyResultDTO;
     }
 
     @Override
     public void delete(Long companyId) throws BusinessValidationException {
         logger.info("delete " + companyId);
+        CompanyResultDTO companyResultDTO = null;
+        for (CompanyResultDTO company: companyResultDTOList) {
+            if (company.getCompanyId().equals(companyId)){
+                companyResultDTO = company;
+            }
+        }
+        if(companyResultDTO != null){
+            companyResultDTOList.remove(companyResultDTO);
+        }
     }
 
     @Override
     public List<CompanyResultDTO> findAll() {
-        List<CompanyResultDTO> customerResultDTOList = new ArrayList<>();
-        return customerResultDTOList;
+        return companyResultDTOList;
     }
 }
